05f4631e031d285d81bb0c4c9d1c29346ede34e9
ayush-chahar <chahar.ayush24@gmail.com>
2024-08-02 21:23:39 +0530

    increased the number of cc to be displayed

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,26 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import millify from 'millify';
 import { Link } from 'react-router-dom';
-import { Card, Row, Col, Input } from 'antd';
+import { Card, Row, Col, Input, Button } from 'antd';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
 import Loader from './Loader';
 
 const Cryptocurrencies = ({ simplified }) => {
-  const count = simplified ? 10 : 100;
+  const count = simplified ? 10 : 1500;
   const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
-  const [cryptos, setCryptos] = useState();
+  const [cryptos, setCryptos] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [displayCount, setDisplayCount] = useState(100);
 
   useEffect(() => {
     setCryptos(cryptosList?.data?.coins);
 
-    const filteredData = cryptosList?.data?.coins.filter((item) => item.name.toLowerCase().includes(searchTerm));
+    const filteredData = cryptosList?.data?.coins.filter((item) =>
+      item.name.toLowerCase().includes(searchTerm)
+    );
 
     setCryptos(filteredData);
   }, [cryptosList, searchTerm]);
 
   if (isFetching) return <Loader />;
+
+  const handleShowMore = () => {
+    setDisplayCount((prevCount) => prevCount + 100);
+  };
 
   return (
     <>
@@ -32,31 +39,36 @@ const Cryptocurrencies = ({ simplified }) => {
           />
         </div>
       )}
-      <Row gutter={[32, 32]} className="crypto-card-container">
-        {cryptos?.map((currency) => (
-          <Col
-            xs={24}
-            sm={12}
-            lg={6}
-            className="crypto-card"
-            key={currency.uuid}
-          >
-
-            {/* Note: Change currency.id to currency.uuid  */}
-            <Link key={currency.uuid} to={`/crypto/${currency.uuid}`}>
-              <Card
-                title={`${currency.rank}. ${currency.name}`}
-                extra={<img className="crypto-image" src={currency.iconUrl} />}
-                hoverable
-              >
-                <p>Price: {millify(currency.price)}</p>
-                <p>Market Cap: {millify(currency.marketCap)}</p>
-                <p>Daily Change: {currency.change}%</p>
-              </Card>
-            </Link>
-          </Col>
-        ))}
-      </Row>
+      <div className="crypto-content">
+        <Row gutter={[32, 32]} className="crypto-card-container">
+          {cryptos?.slice(0, displayCount).map((currency) => (
+            <Col
+              xs={24}
+              sm={12}
+              lg={6}
+              className="crypto-card"
+              key={currency.uuid}
+            >
+              <Link key={currency.uuid} to={`/crypto/${currency.uuid}`}>
+                <Card
+                  title={`${currency.rank}. ${currency.name}`}
+                  extra={<img className="crypto-image" src={currency.iconUrl} />}
+                  hoverable
+                >
+                  <p>Price: {millify(currency.price)}</p>
+                  <p>Market Cap: {millify(currency.marketCap)}</p>
+                  <p>Daily Change: {currency.change}%</p>
+                </Card>
+              </Link>
+            </Col>
+          ))}
+        </Row>
+        {displayCount < cryptos?.length && (
+          <div style={{ textAlign: 'center', marginTop: '20px' }}>
+            <Button onClick={handleShowMore}>Show More</Button>
+          </div>
+        )}
+      </div>
     </>
   );
 };
