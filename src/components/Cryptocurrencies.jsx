143bd79c1dbbea7079bc4b0b9e766ec3cb298a0e
dhruvkej9 <dhruvkejri9@gmail.com>
2024-08-03 15:34:28 +0530

    Show More Design Modified and Improved

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -31,12 +31,14 @@ const Cryptocurrencies = ({ simplified }) => {
 
   return (
     <div className="crypto-container">
-      <div className="search-crypto">
-        <Input
-          placeholder="Search Cryptocurrency"
-          onChange={(e) => setSearchTerm(e.target.value.toLowerCase())}
-        />
-      </div>
+      {!simplified && (
+        <div className="search-crypto">
+          <Input
+            placeholder="Search Cryptocurrency"
+            onChange={(e) => setSearchTerm(e.target.value.toLowerCase())}
+          />
+        </div>
+      )}
       <div className="crypto-content">
         <Row gutter={[32, 32]} className="crypto-card-container">
           {cryptos?.slice(0, displayCount).map((currency) => (
